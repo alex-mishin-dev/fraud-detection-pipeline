848612d34f7ae12400c3b282ec3eee285318574c
alex-mishin-dev <alexandermishin93@gmail.com>
2025-07-03 01:01:19 +0400

    feat: integrate Kafka and Zookeeper into the pipeline
    - Added Kafka and Zookeeper services to docker-compose.
    - Updated data_generator to produce messages to a Kafka topic.
    - Fixed image versions to ensure stable setup.

--- src/data_generator.py
+++ src/data_generator.py
@@ -2,50 +2,43 @@ import pandas as pd
 from faker import Faker
 import random
 from datetime import datetime
-import os
+import json
+from kafka import KafkaProducer
 
-# Создаем экземпляр Faker для генерации фейковых данных
+
 fake = Faker()
+def generate_transactions() -> dict:
+    return {
+        'transaction_id': fake.uuid4(),
+        'customer_id': fake.uuid4(),
+        'amount': round(random.uniform(1.0, 5000.0), 2),
+        'timestamp': datetime.now().isoformat(),
+        'is_fraud': random.choices([0, 1], weights=[95, 5], k=1)[0]
+    }
 
-def generate_transactions(num_transactions: int = 10) -> pd.DataFrame:
-    """
-    Генерирует DataFrame с фейковыми банковскими транзакциями.
 
-    Args:
-        num_transactions (int): Количество транзакций для генерации.
-
-    Returns:
-        pd.DataFrame: DataFrame с данными о транзакциях.
-    """
-    data = []
-    for _ in range(num_transactions):
-        transaction = {
-            'transaction_id': fake.uuid4(),
-            'customer_id': fake.uuid4(),
-            'amount': round(random.uniform(1.0, 5000.0), 2),
-            'timestamp': datetime.now().isoformat(),
-            'is_fraud': random.choices([0, 1], weights=[95, 5], k=1)[0]
-        }
-        data.append(transaction)
-    
-    df = pd.DataFrame(data)
-    return df
-
-# Этот блок выполнится только тогда, когда мы запускаем этот файл напрямую
 if __name__ == "__main__":
     
-    # Определяем путь для сохранения данных ВНУТРИ контейнера
-    output_dir = "/opt/airflow/data"
-    output_path = os.path.join(output_dir, "transactions.csv")
-
-    # Создаем папку data внутри контейнера, если ее нет
-    os.makedirs(output_dir, exist_ok=True)
+    # Создаем продюсера, который будет отправлять сообщения в Kafka
+    producer = KafkaProducer(
+        bootstrap_servers=['localhost:29092'], # Адрес нашего Kafka брокера
+        value_serializer=lambda v: json.dumps(v).encode('utf-8') # Сериализуем сообщения в JSON
+    )
     
-    # Генерируем данные
-    transactions_df = generate_transactions(15)
+    # Название "канала" или "очереди", куда мы будем публиковать транзакции
+    KAFKA_TOPIC = "transactions"
     
-    # Сохраняем DataFrame в CSV-файл
-    transactions_df.to_csv(output_path, index=False)
-    
-    print(f"Сгенерированные транзакции сохранены в {output_path}")
-
+    print("Начинаю отправку транзакций в Kafka. Нажми Ctrl+C для остановки.")
+    try:
+        # Бесконечный цикл для генерации потока данных
+        while True:
+            transaction = generate_transactions()
+            producer.send(KAFKA_TOPIC, value=transaction)
+            print(f"Отправлена транзакция: {transaction['transaction_id']}")
+    except KeyboardInterrupt:
+        print("Остановка генератора.")
+    finally:
+        
+        producer.flush()
+        producer.close()
+        print("Соединение с Kafka закрыто.")
